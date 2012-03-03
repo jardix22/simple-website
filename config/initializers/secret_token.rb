962f3c3a033bfcced62edcbe339ea7484418f081
jardix22 <jardix22@gmail.com>
2012-03-03 10:58:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleWebiste::Application.config.secret_token = '10a57a6de89ba64edb65e60432a2602ed81c5a5374b7bb024464142271f47606272ae7f9d34f7cdfc28fd1336babc5d3a82595d6d3f3442e1df5774c955ceeac'
